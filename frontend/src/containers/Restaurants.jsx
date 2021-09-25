6a430e1c6ab59e75c880e2d3a08069e296a418a0
aspiratio <aspiratio-eng@yahoo.co.jp>
2021-09-25 11:49:50 +0900

    フロントとサーバーサイドのAPI通信実装

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,9 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import { fetchRestaurants } from "../apis/restaurants";
 
 export const Restaurants = () => {
+  useEffect(() => {
+    fetchRestaurants().then((data) => console.log(data));
+  }, []);
   return <Fragment>レストラン一覧</Fragment>;
 };
