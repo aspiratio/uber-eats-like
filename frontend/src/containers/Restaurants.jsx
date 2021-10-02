715c43aa18c6c86a59c6a674b16de0c9da06a846
aspiratio <aspiratio-eng@yahoo.co.jp>
2021-10-02 13:12:42 +0900

    フード一覧ページを作成

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useReducer, useEffect } from "react";
+import { Fragment, useReducer, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
